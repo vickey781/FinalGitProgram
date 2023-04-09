32a688529e132a1a97bae2570a45cbf8371a72db
vicky <amirhussain781@gmail.com>
2023-04-09 15:55:53 +0530

    added implementation for Multiply.java

--- Adding.java
+++ Adding.java
@@ -3,6 +3,6 @@ public class Adding.java{
 public static void main(String []args){
 int a=10;
 int b=20;
-System.out.println(a+b);
+System.out.println("total number of addition is:: "+a+b);
 
 }}
--- Multiply.java
+++ Multiply.java
@@ -1,3 +1,5 @@
 public class Multiply.java{
 
+public static void main(String args[]);
+
 }
